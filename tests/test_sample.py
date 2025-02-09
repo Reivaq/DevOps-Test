f9d6fe6655cd120954e43bc2ee07bf63d5c4be33
Blacvas <ulisesq85@gmail.com>
2025-02-09 19:20:18 +0000

    Initial commit with Dockerfile, cloudbuild.yaml, and deployment.yaml

---
+++ tests/test_sample.py
@@ -0,0 +1,9 @@
+# tests/test_sample.py
+
+def test_addition():
+    """Prueba básica para verificar una suma simple."""
+    assert 1 + 1 == 2
+
+def test_subtraction():
+    """Prueba básica para verificar una resta simple."""
+    assert 5 - 3 == 2
